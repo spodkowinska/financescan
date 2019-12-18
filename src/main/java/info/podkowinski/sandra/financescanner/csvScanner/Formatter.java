7731efa742d9b3e7f035b7aa09e81f3237ac610b
Sandra <spodkowinska@gmail.com>
2019-12-18 22:16:32 +0100

    format of Dates

--- src/main/java/info/podkowinski/sandra/financescanner/csvScanner/Formatter.java
+++ src/main/java/info/podkowinski/sandra/financescanner/csvScanner/Formatter.java
@@ -1,28 +1,27 @@
 package info.podkowinski.sandra.financescanner.csvScanner;
 
 import java.sql.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 public class Formatter  {
 
-    public Date sqlDate (String formatddMMyyyy) {
-       char[] chars = formatddMMyyyy.toCharArray();
-       if(chars.length==10 && (chars[5]>'9'||chars[5]<'0')){
-           StringBuilder sb = new StringBuilder();
-           sb.append(chars[6]);
-           sb.append(chars[7]);
-           sb.append(chars[8]);
-           sb.append(chars[9]);
-           sb.append(chars[5]);
-           sb.append(chars[3]);
-           sb.append(chars[4]);
-           sb.append(chars[2]);
-           sb.append(chars[0]);
-           sb.append(chars[1]);
-           System.out.println(sb.toString());
-           return Date.valueOf(sb.toString());
-       }
-        System.out.println("nie ");
-       return Date.valueOf(formatddMMyyyy);
+    public Date sqlDate (String formatddMMyyyy) throws ParseException {
+
+        if (formatddMMyyyy.getBytes()[2] == '-')
+        {
+            SimpleDateFormat fmt = new SimpleDateFormat("dd-MM-yyyy");
+            return new Date(fmt.parse(formatddMMyyyy).getTime());
+        }
+        else if (formatddMMyyyy.getBytes()[4] == '-')
+        {
+            SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
+            return new Date(fmt.parse(formatddMMyyyy).getTime());
+        }
+        else
+            throw new ParseException("Unrecognized date format", 0);
+
     }
 
 }
