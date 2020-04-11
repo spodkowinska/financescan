56c2857ef5ab511e8ba0148d15a4e4e6d67e2ad7
Sandra <spodkowinska@gmail.com>
2020-04-11 16:55:15 +0200

    x

--- src/main/java/info/podkowinski/sandra/financescanner/project/ProjectController.java
+++ src/main/java/info/podkowinski/sandra/financescanner/project/ProjectController.java
@@ -6,11 +6,17 @@ import info.podkowinski.sandra.financescanner.category.CategoryService;
 import info.podkowinski.sandra.financescanner.csvScanner.CsvSettings;
 import info.podkowinski.sandra.financescanner.csvScanner.CsvSettingsService;
 import info.podkowinski.sandra.financescanner.transaction.TransactionService;
+import info.podkowinski.sandra.financescanner.user.User;
+import info.podkowinski.sandra.financescanner.user.UserService;
+import info.podkowinski.sandra.financescanner.user.UserServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Controller
 @RequestMapping("/project")
@@ -21,21 +27,30 @@ public class ProjectController {
     private final AccountService accountService;
     private final CategoryService categoryService;
     private final CsvSettingsService csvSettingsService;
+    private final UserServiceImpl userService;
 
     public ProjectController(TransactionService transactionService, ProjectService projectService, AccountService accountService,
-                                 CategoryService categoryService, CsvSettingsService csvSettingsService) {
+                             CategoryService categoryService, CsvSettingsService csvSettingsService, UserServiceImpl userService) {
         this.transactionService = transactionService;
         this.projectService = projectService;
         this.accountService = accountService;
         this.categoryService = categoryService;
         this.csvSettingsService = csvSettingsService;
+        this.userService = userService;
     }
 
     @GetMapping("/set")
-    public String setProject(){
-            Long projectId = projectService.createDefaultProject();
-            categoryService.createDefaultCategories(projectId);
-            csvSettingsService.createDefaultBanksSettings();
-            return "redirect:../category/list";
-        }
+    public String setProject() {
+        Long projectId = projectService.createDefaultProject();
+        categoryService.createDefaultCategories(projectId);
+        csvSettingsService.createDefaultBanksSettings();
+        User user = new User();
+        user.setPassword("user");
+        user.setUsername("user");
+        List<Project> project1= new ArrayList<>();
+        project1.add(projectService.findById(projectId));
+        user.setProjects(project1);
+        userService.saveUser(user);
+        return "redirect:../category/list";
+    }
 }
