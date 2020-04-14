8cb784a02f7d9e081c0978adb24109876101c54e
podkova <podkovax@gmail.com>
2020-04-14 21:20:45 +0200

    User: work on login/register/forgot password * added mappings for 'register' and 'forgot password' pages * added 'user' subdirectory for user views * renamed UserController-related files to conform to the other views naming * removed unused entry from WebAppConfig

--- src/main/java/info/podkowinski/sandra/financescanner/config/WebAppConfig.java
+++ src/main/java/info/podkowinski/sandra/financescanner/config/WebAppConfig.java
@@ -8,7 +8,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebAppConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("/login").setViewName("login");
         registry.addViewController("/403").setViewName("403");
     }
 
