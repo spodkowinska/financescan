07d54fe21ebc9b7c8b23b96d973db6d2440685f1
podkova <podkovax@gmail.com>
2020-04-26 16:14:34 +0200

    reports.js refactor: from now on months are loaded in ascending order instead of random

--- src/main/webapp/js/reports.js
+++ src/main/webapp/js/reports.js
@@ -24,66 +24,68 @@ function setYear(year, sender) {
 
     $('.finance_table .month').css({ 'font-weight' : 'initial', 'text-decoration' : 'initial' });
 
-    let monthsCompleted = 0;
-    let monthsValid = 0.0;
-
     $('.finance_table td, .finance_table th').removeClass('negative positive unused').filter('td').text('');
 
-    for (let monthIndex = 0; monthIndex < 12; monthIndex++) {
-        $.get('/report/' + year + '/' + (monthIndex + 1), function (data) {
-            const month = JSON.parse(data);
+    fillMonth(0, 0);
+}
 
-            // Set current month if needed
-            if (month.current)
-                $('th.month_' + monthIndex).css({ 'font-weight' : 'bold', 'text-decoration' : 'underline' });
+function fillMonth(monthIndex, monthsValid) {
+    $.get('/report/' + gCurrentYear + '/' + (monthIndex + 1), function (data) {
+        const month = JSON.parse(data);
 
-            if (month.valid) {
-                monthsValid += 1.0;
+        // Set current month if needed
+        if (month.current)
+            $('th.month_' + monthIndex).css({ 'font-weight' : 'bold', 'text-decoration' : 'underline' });
 
-                // Fill column with zeros
-                $('td.month_' + monthIndex).text('0');
+        if (month.valid) {
+            monthsValid++;
 
-                // Fill given cells with amounts
-                for (const cat in month.cats) {
-                    const id = month.cats[cat].id;
-                    const balance = month.cats[cat].balance;
-                    setAmountForCell(balance, $('#month_' + monthIndex + '_cat_' + id));
-                }
+            // Fill column with zeros
+            $('td.month_' + monthIndex).text('0');
 
-                // Fill sum
-                setAmountForCell(month.balance, $('#month_'  + monthIndex + '_sum'));
-            }
-            else {
-                $('.month_' + monthIndex).addClass('unused');
+            // Fill given cells with amounts
+            for (const cat in month.cats) {
+                const id = month.cats[cat].id;
+                const balance = month.cats[cat].balance;
+                setAmountForCell(balance, $('#month_' + monthIndex + '_cat_' + id));
             }
 
-            monthsCompleted++;
+            // Fill sum
+            setAmountForCell(month.balance, $('#month_'  + monthIndex + '_sum'));
+        }
+        else {
+            $('.month_' + monthIndex).addClass('unused');
+        }
 
-            if (monthsCompleted === 12) {
-                $.get('/report/' + year, function (data) {
+        monthIndex++;
 
-                    if (monthsValid === 0)
-                        monthsValid = 1;
+        if (monthIndex < 12) {
+            fillMonth(monthIndex, monthsValid);
+        }
+        else if (monthIndex === 12) {
+            $.get('/report/' + gCurrentYear, function (data) {
 
-                    const year = JSON.parse(data);
+                if (monthsValid === 0)
+                    monthsValid = 1;
 
-                    // Fill sum and avg
-                    setAmountForCell(year.balance, $('#sum_sum'));
-                    setAmountForCell(year.balance/monthsValid, $('#avg_sum'));
+                const year = JSON.parse(data);
 
-                    $('.finance_table tbody tr').each(function() {
+                // Fill sum and avg
+                setAmountForCell(year.balance, $('#sum_sum'));
+                setAmountForCell(year.balance/monthsValid, $('#avg_sum'));
 
-                        // Fill column with avg amount
-                        const id = $(this).data('category-id');
-                        setAmountForCell(
-                            year.cats[id] ? year.cats[id].balance : 0,
-                            $(this).children().filter('.sum'));
-                        setAmountForCell(
-                            year.cats[id] ? year.cats[id].balance/monthsValid : 0,
-                            $(this).children().filter('.avg'));
-                    });
+                $('.finance_table tbody tr').each(function() {
+
+                    // Fill column with avg amount
+                    const id = $(this).data('category-id');
+                    setAmountForCell(
+                        year.cats[id] ? year.cats[id].balance : 0,
+                        $(this).children().filter('.sum'));
+                    setAmountForCell(
+                        year.cats[id] ? year.cats[id].balance/monthsValid : 0,
+                        $(this).children().filter('.avg'));
                 });
-            }
-        });
-    }
+            });
+        }
+    });
 }
